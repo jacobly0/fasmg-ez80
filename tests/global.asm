4c6dd7608462662d3f740aae81ad422d23627191
Jacob Young <jacobly0@users.noreply.github.com>
2017-10-02 14:32:35 -0400

    Oh, it was already like that.

--- tests/global.asm
+++ tests/global.asm
@@ -1,4 +1,4 @@
-format.ti executable protected program 'GLOBAL'
+format ti executable protected program 'GLOBAL'
 files global_first.src global_second.src
 locate CODE at $D1A881
 include 'ld.fasmg'
